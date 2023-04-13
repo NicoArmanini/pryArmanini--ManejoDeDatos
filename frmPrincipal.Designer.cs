cd23422dce80fd867ea08d22f9390a605202445d
root <mgiraudo@ies21.edu.ar>
2023-04-13 13:51:11 -0300

    Agregar archivos de proyecto.

---
+++ frmPrincipal.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace pryArmanini__ManejoDeDatos
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDatos = new System.Windows.Forms.Label();
+            this.txtDatoABuscar = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblDatos
+            // 
+            this.lblDatos.AutoSize = true;
+            this.lblDatos.Location = new System.Drawing.Point(34, 29);
+            this.lblDatos.Name = "lblDatos";
+            this.lblDatos.Size = new System.Drawing.Size(35, 13);
+            this.lblDatos.TabIndex = 0;
+            this.lblDatos.Text = "label1";
+            // 
+            // txtDatoABuscar
+            // 
+            this.txtDatoABuscar.Location = new System.Drawing.Point(231, 29);
+            this.txtDatoABuscar.Name = "txtDatoABuscar";
+            this.txtDatoABuscar.Size = new System.Drawing.Size(140, 20);
+            this.txtDatoABuscar.TabIndex = 1;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(443, 32);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(93, 35);
+            this.btnBuscar.TabIndex = 2;
+            this.btnBuscar.Text = "buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 420);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtDatoABuscar);
+            this.Controls.Add(this.lblDatos);
+            this.Name = "frmPrincipal";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDatos;
+        private System.Windows.Forms.TextBox txtDatoABuscar;
+        private System.Windows.Forms.Button btnBuscar;
+    }
+}
+
---
+++ frmPrincipal.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace pryArmanini__ManejoDeDatos
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+            //sacar datos de un archivo
+
+
+            //ABRIR O CREAR UN ARCHIVO
+           // StreamWriter manejoArchivo = new StreamWriter("LIBRO1.txt");
+
+            StreamReader leerArchivo = new StreamReader("LIBRO1.txt");
+
+            while (leerArchivo.EndOfStream == false)
+            {
+               
+                lblDatos.Text += leerArchivo.ReadLine() + "/n";
+                //lblDatos.Text = lblDatos.Text + leerArchivo.ReadLine();
+                //es lo mismo que lo de arriba
+            }
+
+            leerArchivo.Close();
+           // manejoArchivo.Close();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+
+            StreamReader leerArchivo = new StreamReader("LIBRO1.txt");
+
+            while (leerArchivo.EndOfStream == false)
+            {
+                string auxiliar = leerArchivo.ReadLine();
+                //buscar dentro del stream / texto
+                if (auxiliar.Contains(txtDatoABuscar.Text))
+                {
+                    lblDatos.Text = "Encntrado";
+                    lblDatos.Text += auxiliar;
+                    break;
+                }
+                else
+                {
+                    lblDatos.Text = "NO PASA NARANJA";
+                }
+            }
+
+            leerArchivo.Close();
+            
+        }
+    }
+}
